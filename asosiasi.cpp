d20dc4dcc9a67e7ae1d437f87decee9c7e50b27d
rnktlhr <rinakitluhur10@gmail.com>
2024-05-20 17:12:30 +0700

    menambahkan class dokter dan class pasien beserta member dan function

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
 
+class dokter;
+class pasien {
+    public: 
+        string nama;
+        vector<dokter*> daftar_dokter;
+        pasien(string pNama){
+            cout << "Pasien\"" << nama << "\" ada\n";
+
+        }
+        ~pasien(){
+            cout << "Pasien \"" << nama << "\" tidak ada\n";
+        }
+
+        void tambahDokter(dokter*);
+        void cetakDokter();
+};
 int main(){
     
 }
