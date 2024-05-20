a388cda20b4a9b67a1ca506c011dc464f860a7b7
rnktlhr <rinakitluhur10@gmail.com>
2024-05-20 15:55:33 +0700

    menambahkan class manusia

--- composisi/manusia.h
+++ composisi/manusia.h
@@ -2,3 +2,17 @@
 #define MANUSIA_H
 
 using namespace std;
+
+class manusia{
+    public: 
+        string name;
+        jantung varJantung;
+
+        manusia(string pName):name(pName){
+            cout << name << " hidup\n";
+        }
+        ~manusia(){
+            cout << name << " mati\n";
+        }
+};
+#endif
