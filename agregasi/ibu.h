ce3f4d7436029aecba9126a015f80a8df46b2d4f
rnktlhr <rinakitluhur10@gmail.com>
2024-05-20 15:10:24 +0700

    menambahkan void ibu :: cetak anak

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -22,3 +22,11 @@ class ibu{
 void ibu::tambahAnak(anak* pAnak){
     daftar_anak.push_back(pAnak);
 }
+
+void ibu::cetakAnak(){
+    cout << "Daftar Anak dari ibu \"" << this->nama << "\":\n";
+    for(auto& a : daftar_anak){
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
