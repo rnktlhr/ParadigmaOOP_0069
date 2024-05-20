5a69568c45eea9ca5549a97ea6d75784523a52c2
rnktlhr <rinakitluhur10@gmail.com>
2024-05-20 15:03:58 +0700

    menambahkan member

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -1,3 +1,15 @@
 #ifndef IBU_H
 #define IBU_H
 
+class ibu{
+    public:
+        string nama;
+        vector<anak*> daftar_anak;
+
+        ibu(string pNama):nama(pNama){
+            cout << "Ibu \"" << nama << "\" ada\n";
+        }
+
+        
+};
+    
